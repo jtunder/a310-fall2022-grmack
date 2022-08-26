6962c20b5ae1ea0190f7afe0824b696df364c4e9
gabe <gabemack@users.noreply.github.com>
2022-08-26 11:52:45 -0400

    Create driver.py
    Circle is not implemented

---
+++ 0826/driver.py
@@ -0,0 +1,36 @@
+import turtle
+
+def main():
+    timmy = turtle.Turtle()
+    screen = timmy.getscreen()
+    filename = input("Insert filename: ")
+    file = open(filename, "r")
+
+    for line in file:
+        text = line.strip()
+        commandList = text.split(",")
+        command = commandList[0]
+
+        if command == "goto":
+            x = float(commandList[1])
+            y = float(commandList[2])
+            width = float(commandList[3])
+            color = commandList[4].strip()
+            timmy.width(width)
+            timmy.pencolor(color)
+            timmy.goto(x,y)
+        elif command == "penup":
+            timmy.penup()
+        elif command == "pendown":
+            timmy.pendown()
+        elif command == "beginfill":
+            color = commandList[1].strip()
+            t.fillcolor(color)
+            t.begin_fill()
+        elif command == "endfill":
+            t.end_fill()
+
+        
+        
+if __name__ == "__main__":
+    main()
